4decb0e818afd166d4a1e08736e84dd8671f361d
Pauline <pchan1@umd.edu>
2017-03-25 20:02:24 -0400

    Request page form validation - started

--- client/patient/main.js
+++ client/patient/main.js
@@ -9,28 +9,29 @@ Template.main_page.onRendered(function(){
 			
 		}
 		this.$('#loginForm').form({
-			inline: true,
-			on: 'blur',
-			transition: 'slide down',
-			onSuccess: function(event,fields ){
-				event.preventDefault()
-		
-				Meteor.loginWithPassword(fields['email'],fields['password'],(error,result)=>{
-					if(error){
-						$('#error_message').show()
-						$('#error_message').html("<div class='header'>" + 'Please try again' + '</div>' + '<p>' + error['reason'] + "</p>")
-					}
-					else{
-						console.log('Login was successful',result);
-						FlowRouter.go('/dashboard')
-					}
-				})
+		inline: true,
+		on: 'blur',
+		transition: 'slide down',
+		onSuccess: function(event,fields ){
+			event.preventDefault()
+	
+			Meteor.loginWithPassword(fields['email'],fields['password'],(error,result)=>{
+				if(error){
+					$('#error_message').show()
+					$('#error_message').html("<div class='header'>" + 'Please try again' + '</div>' + '<p>' + error['reason'] + "</p>")
+				}
+				else{
+					console.log('Login was successful',result);
+					FlowRouter.go('/dashboard')
+				}
+			})
 
-			},
-			fields: {
-				email: {
-					identifier : 'email',
-					rules: [
+		},
+		fields: {
+			email: {
+				identifier : 'email',
+				rules: 
+				[
 					{
 						type: 'empty',
 						prompt: 'Please enter your e-mail'
@@ -39,23 +40,24 @@ Template.main_page.onRendered(function(){
 						type: 'email',
 						prompt: 'Please enter a valid e-mail'
 					}
-					]
-				},
-				password: {
-					identifier : 'password',
-					rules: [
-						{
-							type: 'empty',
-							prompt: 'Please enter your password'
-						},
-						{
-							type: 'length[6]',
-							prompt: 'Your password must be at least 6 characters'
-						}
-					]
+				]
+			},
+			password: {
+				identifier : 'password',
+				rules: 
+				[
+					{
+						type: 'empty',
+						prompt: 'Please enter your password'
+					},
+					{
+						type: 'length[6]',
+						prompt: 'Your password must be at least 6 characters'
+					}
+				]
 			}
-		}}
-		)
+		}
+	});
 
 	// Handle for account creation.
 	/*
--- client/patient/requestPage.js
+++ client/patient/requestPage.js
@@ -2,7 +2,7 @@ import '../../lib/collections.js'
 
 Template.requestPage.onRendered(
 	function(){
-		$("#relationship-form").toggle().hide();
+		$("#relationship-form").hide();
 
 		$("#myonoffswitch").click(function(){
 			$('#relationship-form').toggle();
@@ -10,7 +10,7 @@ Template.requestPage.onRendered(
 
 		// Click billing address toggle
 		$("#myonoffswitch-address").click(function(){
-			$('#address-form').toggle();
+			$('#address_form').toggle();
 		});
 
 		// Click billing address toggle
@@ -24,37 +24,180 @@ Template.requestPage.onRendered(
 
 		this.$('.ui.form').form({
 			inline: true,
+			on: 'blur',
+			transition: 'slide down',
 			onSuccess: function(event,fields){
-				event.preventDefault()
+				event.preventDefault();
+
+				////////////////// ADDS TO DATABASE ///////////////////////
 				Meteor.call('updateStatus', Meteor.userId())
 				Meteor.call('pushRequest', Meteor.userId(), fields)
-				FlowRouter.go('/dashboard')
+				//////////////////////////////////////////////////////////
+
+				FlowRouter.go('/dashboard') // Redirects to dashboard
 			},
-			fields: 
-			{
-				symptoms:
+
+			/////////////////////// FORM VALIDATION //////////////////////
+			fields: {	
+				relationship: {
+					identifier: 'relationship',
+					depends: 'for_someone_else',
+					rules:[
 					{
-						identifier: 'symptoms',
-						rules:
-						[
-							{
-								type: 'empty',
-								prompt: 'Please let us know your symptoms.'
-							}
-						]
-					},
-				relationship:
-					{
-						identifier: 'relationship',
-						depends: 'for_someone_else',
-						rules:
-						[
-							{
-								type: 'empty',
-								prompt: "Pleaase specify your relationship."
-							}
-						]
+						type: 'empty',
+						prompt: "Pleaase specify your relationship."
 					}
+					]
+				},
+				street:{
+	                identifier: 'street',
+	                required: {
+						depends: function(element) {
+							if ($('#myonoffswitch-address').is(':checked')){
+								return false;
+						}else{
+								return true;
+						}
+						}
+					},
+	                rules:[
+	                {
+	                    type: 'empty',
+	                    prompt: 'Please enter a street address.'
+	                }
+	                ]
+	            },
+	            city:{
+	                identifier: 'city',
+	                required: {
+						depends: function(element) {
+							if ($('#myonoffswitch-address').is(':checked')){
+								return false;
+						}else{
+								return true;
+						}
+						}
+					},
+	                rules:[
+	                {
+	                    type: 'regExp[/^[A-Za-z]{0,144}$/]',
+	                    prompt: 'Please enter a valid city.'
+	                },
+	                {
+	                    type: 'empty',
+	                    prompt: 'Please enter a city.'
+	                }
+	                ]
+	            },
+	            state:{
+	                identifier: 'state',
+	                required: {
+						depends: function(element) {
+							if ($('#myonoffswitch-address').is(':checked')){
+								return false;
+						}else{
+								return true;
+						}
+						}
+					},
+	                rules:[
+	                {
+	                    type: 'empty',
+	                    prompt: 'Please enter a state.'
+	                }
+	                ]
+	            },
+	            country:{
+	                identifier: 'country',
+	                required: {
+						depends: function(element) {
+							if ($('#myonoffswitch-address').is(':checked')){
+								return false;
+						}else{
+								return true;
+						}
+						}
+					},
+	                rules:[
+	                {
+	                    type: 'empty',
+	                    prompt: 'Please choose a country.'
+	                }
+	                ]
+	            },
+	            zipcode:{
+	                identifier: 'zipcode',
+	                required: {
+						depends: function(element) {
+							if ($('#myonoffswitch-address').is(':checked')){
+								return false;
+						}else{
+								return true;
+						}
+						}
+					},
+	                rules: [
+	                {
+	                    type: 'empty',
+	                    prompt: 'Please enter a zip code.'
+	                }
+	                ]
+	            },
+	            aptNumSuite:{
+	                identifier: 'aptNumSuite',
+	                optional: true,
+	                rules: [
+	                {
+	                    type: 'empty',
+	                    prompt: 'Please enter an apartment or suite number.'
+	                }
+	                ]
+	            },
+	            date:{
+	                identifier: 'date',
+	                required: {
+						depends: function(element) {
+							if ($('#myonoffswitch-time').is(':checked')){
+								return false;
+						}else{
+								return true;
+						}
+						}
+					},
+	                rules:[
+	                {
+	                    type: 'empty',
+	                    prompt: 'Please choose a date.'
+	                }
+	                ]
+	            },
+	            time:{
+	                identifier: 'time',
+	                required: {
+						depends: function(element) {
+							if ($('#myonoffswitch-time').is(':checked')){
+								return false;
+						}else{
+								return true;
+						}
+						}
+					},
+	                rules: [
+	                {
+	                    type: 'empty',
+	                    prompt: 'Please choose a time.'
+	                }
+	                ]
+	            },
+				symptoms: {
+					identifier: 'symptoms',
+					rules: [
+					{
+						type: 'empty',
+						prompt: 'Please let us know your symptoms.'
+					}
+					]
+				}
 			}
 		})
 	}
