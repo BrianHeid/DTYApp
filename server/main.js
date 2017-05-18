b237cc8ebc2392093a3306fc4777399539e71409
Melanie Kaplan-Cohen <mkaplancohen@gmail.com>
2017-05-18 11:01:18 -0400

    changed env.mail_url, FORGOT PASSWORD IS WORKING!!!!!!!!!!!!!!!

--- server/main.js
+++ server/main.js
@@ -6,7 +6,7 @@ Meteor.startup(() => {
   // code to run on server at startup
 //export const Documents = new Mongo.Collection('User_Documents')
 //export const Reviews = new Mongo.Collection('Reviews')
-process.env.MAIL_URL = 'smtp://postmaster%40sandboxbab1a08279c84b34a1c96809281db21c.mailgun.org:7dfad06025cf2029c89f75b5c80e7b25@smtp.mailgun.org:587'
+process.env.MAIL_URL = 'smtp://postmaster%40sandboxc325b32bfc38422ea41238133f509d15.mailgun.org:ff6851dffef29d15f252de3ae979c60c@smtp.mailgun.org:587'
 // Meteor.call('email','DoxToYou@gmail.com','DoxToYou@gmail.com','Test','Test 2 message')
 });
 
