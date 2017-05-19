ac12cce1b711cdd1e7d2f476e2604e80ddd8393e
Ben Call <benjamin.ncall@gmail.com>
2017-05-19 19:02:22 -0400

    added server and settings code

---
+++ server/braintree_config.js
@@ -0,0 +1,33 @@
+// Define gateway variable
+var gateway;
+
+Meteor.startup(function () {
+  var env;
+  // Pick Braintree environment based on environment defined in Meteor settings.
+  if (Meteor.settings.public.env === 'Production') {
+    env = Braintree.Environment.Production;
+  } else {
+    env = Braintree.Environment.Sandbox;
+  }
+  // Initialize Braintree connection:
+  gateway = BrainTreeConnect({
+    environment: env,
+    publicKey: Meteor.settings.public.BT_PUBLIC_KEY,
+    privateKey: Meteor.settings.private.BT_PRIVATE_KEY,
+    merchantId: Meteor.settings.public.BT_MERCHANT_ID
+  });
+});
+
+Meteor.methods({
+  getClientToken: function (clientId) {
+    var generateToken = Meteor.wrapAsync(gateway.clientToken.generate, gateway.clientToken);
+    var options = {};
+
+    if (clientId) {
+      options.clientId = clientId;
+    }
+
+    var response = generateToken(options);
+    return response.clientToken;
+  }
+});
