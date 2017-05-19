4e58ac8e2ea6cb6a85187e797b295b9b8c283c59
Ben Call <benjamin.ncall@gmail.com>
2017-05-19 17:52:35 -0400

    two more texts

--- client/patient/treatmentPage.js
+++ client/patient/treatmentPage.js
@@ -1,14 +1,14 @@
 Template.treatmentPage.onRendered(function(){
-    
+
     ////// NEED TO PULL PROVIDER PROFILE PICTURE FROM DB
     var imageSrc = "/images/drbrown.jpg";
     var imageHTML = "<img src=\"" + imageSrc + "\">";
     document.getElementById("imageProvider").innerHTML = imageHTML;
- 
-        
+
+
     // Click to open cancel request button
     $(".cancelBtn").click(function(){
-        $('#reasonModal').modal('show');	
+        $('#reasonModal').modal('show');
     });
 
     // Validate cancel form
@@ -32,7 +32,7 @@ Template.treatmentPage.onRendered(function(){
                 Meteor.call('resetStatus', Meteor.userId());
             });
 
-            
+
         },
         fields: {
             reason: {
@@ -52,40 +52,50 @@ Template.treatmentPage.onRendered(function(){
             }
         }
     });
-    
-    var emailAddress = Meteor.users.findOne({_id:Meteor.userId()}).emails[0].address;
+
+  var emailAddress = Meteor.users.findOne({_id:Meteor.userId()}).emails[0].address;
 	Meteor.subscribe("currProfile", emailAddress);
 	Meteor.subscribe("currRequest", Meteor.userId());
-	
+
 	var providerId = Requests.findOne().providerId;
 	Meteor.subscribe("currProvider", providerId);
-    
+
     $("#confirmCancelBtn").click(function(event){
 		var id = Requests.findOne()._id;
 		var reason = $("#reason").innerHTML;
+
+    var emailAddress = Meteor.users.findOne({_id:Meteor.userId()}).emails[0].address;
+    var willSendSMS = Profiles.findOne({email: emailAddress}).preferences;
+
+    if (willSendSMS) {
+      Meteor.call('sendSMS',{
+        to: '+1' + Profiles.findOne({email: emailAddress}).phone,
+        text: 'Doctors To You Update: Request Cancelled.'
+      });
+    }
 		Meteor.call('cancelRequest', id, reason, true);
 	});
-    
+
     // Multi-select dropdown functionality
     $('#multi-select')
       .dropdown()
-    ; 
+    ;
 });
 
 Template.treatmentPage.helpers({
    providerName: function(){
         var providerFullName = "";
 		var isDoctor = Providers.findOne().isDoctor;
-		
+
 		if (isDoctor) {
             providerFullName += "Dr. ";
         }
 		var providerId = Requests.findOne().providerId;
 		var firstName = Profiles.findOne({_id:providerId}).firstname;
 		var lastName = Profiles.findOne({_id:providerId}).lastname;
-		
+
 		providerFullName += firstName + " " + lastName;
-		
+
 		return providerFullName;
    },
    ETA: function(){
@@ -94,5 +104,5 @@ Template.treatmentPage.helpers({
    patientName: function(){
 		return Profiles.findOne().firstname;
    }
-   
+
 });
